e2f702b862e6c13c8f17696150e012dc394588f3
AlukweClaid <claidalukwe@gmail.com>
2025-11-28 12:13:41 +0300

    changing view message

--- Djangoproject/firstapp/views.py
+++ Djangoproject/firstapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def ourfirstapp(request):
-    return HttpResponse("This is our first app in Django")
+    return HttpResponse("Hello World welcome to our first app")
