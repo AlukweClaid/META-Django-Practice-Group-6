a3414ac32abb1b382f602893db50b11313260424
AlukweClaid <claidalukwe@gmail.com>
2025-11-29 08:22:39 +0300

    update and delete operations

--- Djangoproject/firstapp/urls.py
+++ Djangoproject/firstapp/urls.py
@@ -3,10 +3,21 @@ from . import views
 
 urlpatterns =[
     path('', views.home, name='home'),
-    path('create-shoes/', views.createShoes,name='create-shoes'),
-    path('create-clothes/', views.createClothes,name='create-clothes'),
-     path('shoes',views.fetchAllShoes,name='fetchAll')
-    
+    path('create-shoes/', views.createShoes, name='create-shoes'),
+    path('create-clothes/', views.createClothes, name='create-clothes'),
+    path('hello/', views.ourfirstapp, name='hello'),
+
+    # Shoes CRUD
+    path('shoes/', views.fetchAllShoes, name='fetchAll'),
+    path('shoes/<int:pk>/', views.shoes_detail, name='shoes-detail'),
+    path('shoes/<int:pk>/edit/', views.updateShoes, name='update-shoes'),
+    path('shoes/<int:pk>/delete/', views.deleteShoes, name='delete-shoes'),
+
+    # Clothes CRUD (optional)
+    path('clothes/', views.fetchAllClothes, name='fetchAllClothes'),
+    path('clothes/<int:pk>/', views.clothes_detail, name='clothes-detail'),
+    path('clothes/<int:pk>/edit/', views.updateClothes, name='update-clothes'),
+    path('clothes/<int:pk>/delete/', views.deleteClothes, name='delete-clothes'),
 ]
 
     
